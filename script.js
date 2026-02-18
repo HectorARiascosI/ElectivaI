3c3540063d66136138c6bfd07228ccaa0167c406
HectorARiascosIy <hector.riascosins@campusucc.edu.co>
2026-02-17 23:20:34 -0500

    Requerimiento 5: modo oscuro y animación interactiva

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const boton = document.getElementById("infoBtn");
 const mensaje = document.getElementById("mensaje");
+const temaBtn = document.getElementById("temaBtn");
 
 boton.addEventListener("click", function() {
     mensaje.textContent = "El jaguar tiene la mordida más poderosa entre los felinos.";
+    mensaje.classList.add("mostrar");
+});
+
+temaBtn.addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
 });
